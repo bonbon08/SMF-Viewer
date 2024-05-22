60d7129aabde942a999560a076c6d2bc7ce9533f
bonbon08 <mail@jan-schmitt.eu>
2024-05-22 10:29:59 +0200

    added color mode

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use std::{fs, thread, time::Duration};
+use std::{fs, thread, time::Duration, env};
 use gfxsrc;
 
 fn main() {
-    let path = "test.smf".trim();
+    let args : Vec<String> = env::args().collect();
+    let mut color = "#FFFFFF";
+    let mut repeat = false;
+    for i in &args {
+        let u = i.split('=').collect::<Vec<&str>>();
+        if u[0] == "--loop" {
+            repeat = true;
+        } else if u[0] == "--matrixcolor" {
+            color = u[1];
+        }
+    }
+    let path = &args[args.len()-1];
     let content = fs::read_to_string(path).expect("Path not found");
     let mut app = gfxsrc::Screen::new(16, 8, ' '.to_string());
     for line in content.lines() {
@@ -16,13 +27,13 @@ fn main() {
         } else if parts[0] == "ON" {
             for i in 1..8{
                 for u in 1..8{
-                    app.addstring(i*2, u, "#", "#FFFFFF")
+                    app.addstring(i*2, u, "#", &color)
                 }
             }
         } else {
             for pixel in parts[0].split(',') {
                 let possitions = pixel.split_whitespace().collect::<Vec<&str>>();
-                app.addstring(((possitions[0].parse::<usize>().unwrap())+1)*2,(possitions[1].parse::<usize>().unwrap())+1, "#", "#FFFFFF");
+                app.addstring(((possitions[0].parse::<usize>().unwrap())+1)*2,(possitions[1].parse::<usize>().unwrap())+1, "#", &color);
             }
         }
         app.print();
