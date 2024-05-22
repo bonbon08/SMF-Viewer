d059c236ac9d244b6ab2f0fc7c411f591e5985ea
bonbon08 <mail@jan-schmitt.eu>
2024-05-22 11:15:38 +0200

    Cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,92 @@
-use std::{fs, thread, time::Duration, env};
-use gfxsrc;
+use std::{env::{self}, fs, thread, time::Duration};
+use gfxsrc::{self, Screen};
 
-fn main() {
-    let args : Vec<String> = env::args().collect();
-    let mut color = "#FFFFFF";
+struct Matrix{
+    color: Box<str>,
+    repeat:bool,
+    app: Screen,
+    content: String,
+}
+
+fn prepare_matrix() -> Matrix {
+    let args: Vec<String> = env::args().collect();
+    let mut color = "#FFFFFF".into();
     let mut repeat = false;
     for i in &args {
         let u = i.split('=').collect::<Vec<&str>>();
         if u[0] == "--loop" {
             repeat = true;
         } else if u[0] == "--matrixcolor" {
-            color = u[1];
+            color = u[1].into();
         }
     }
     let path = &args[args.len()-1];
     let content = fs::read_to_string(path).expect("Path not found");
-    let mut app = gfxsrc::Screen::new(16, 8, ' '.to_string());
-    for line in content.lines() {
-        let parts = line.split('-').collect::<Vec<&str>>();
-        if parts[0] == "OFF" {
-            for i in 1..8{
-                for u in 1..8{
-                    app.addstring(i*2, u, " ", "#FFFFFF")
+    Matrix{
+        color: color,
+        repeat: repeat,
+        app: gfxsrc::Screen::new(16, 8, ' '.to_string()),
+        content: content,
+    }
+}
+
+
+fn showmatrix(mut matrix: Matrix){
+    if matrix.repeat == true {
+        loop {
+            for line in matrix.content.lines() {
+                let parts = line.split('-').collect::<Vec<&str>>();
+                if parts[0] == "OFF" {
+                    for i in 1..8{
+                        for u in 1..8{
+                            matrix.app.addstring(i*2, u, " ", "#FFFFFF")
+                        }
+                    }
+                } else if parts[0] == "ON" {
+                    for i in 1..8{
+                        for u in 1..8{
+                            matrix.app.addstring(i*2, u, "#", &matrix.color)
+                        }
+                    }
+                } else {
+                    for pixel in parts[0].split(',') {
+                        let possitions = pixel.split_whitespace().collect::<Vec<&str>>();
+                        matrix.app.addstring(((possitions[0].parse::<usize>().unwrap())+1)*2,(possitions[1].parse::<usize>().unwrap())+1, "#", &matrix.color);
+                    }
                 }
-            }
-        } else if parts[0] == "ON" {
-            for i in 1..8{
-                for u in 1..8{
-                    app.addstring(i*2, u, "#", &color)
-                }
-            }
-        } else {
-            for pixel in parts[0].split(',') {
-                let possitions = pixel.split_whitespace().collect::<Vec<&str>>();
-                app.addstring(((possitions[0].parse::<usize>().unwrap())+1)*2,(possitions[1].parse::<usize>().unwrap())+1, "#", &color);
+                matrix.app.print();
+                thread::sleep(Duration::from_millis(parts[1].parse::<u64>().unwrap()));
             }
         }
-        app.print();
-        thread::sleep(Duration::from_millis(parts[1].parse::<u64>().unwrap()));
+    } else {
+        for line in matrix.content.lines() {
+            let parts = line.split('-').collect::<Vec<&str>>();
+            if parts[0] == "OFF" {
+                for i in 1..8{
+                    for u in 1..8{
+                        matrix.app.addstring(i*2, u, " ", "#FFFFFF")
+                    }
+                }
+            } else if parts[0] == "ON" {
+                for i in 1..8{
+                    for u in 1..8{
+                        matrix.app.addstring(i*2, u, "#", &matrix.color)
+                    }
+                }
+            } else {
+                for pixel in parts[0].split(',') {
+                    let possitions = pixel.split_whitespace().collect::<Vec<&str>>();
+                    matrix.app.addstring(((possitions[0].parse::<usize>().unwrap())+1)*2,(possitions[1].parse::<usize>().unwrap())+1, "#", &matrix.color);
+                }
+            }
+            matrix.app.print();
+            thread::sleep(Duration::from_millis(parts[1].parse::<u64>().unwrap()));
+        }
     }
+}
+
+
+fn main() {
+    let matrix = prepare_matrix();
+    showmatrix(matrix);
 }
